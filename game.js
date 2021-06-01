ff8d80eb620567e1f7bb43908d46ce35bb190a58
lucaseduardopedroso <lucaseduardosilvapedroso@gmail.com>
2021-05-31 23:46:53 -0300

    Add button press checks and sounds

--- game.js
+++ game.js
@@ -1,8 +1,17 @@
 var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
+var userClickedPattern = [];
+
+//Check the button click and store its color (id)
+$(".btn").click(function(){
+    var userChosenColour = $(this).attr('id');
+    //Store the color clicked in userClickedPattern
+    userClickedPattern.push(userChosenColour);
+    playSound(userChosenColour);
+});
 
 function nextSequence(){
-    //Generate a number between 0 - 3 and store it.
+    //Generate a number between 0 - 3 and store it
     var randomNumber = Math.floor(Math.random() * 4);
     //Choose a random color from buttonColours using the random number
     var randomChosenColour = buttonColours[randomNumber];
@@ -12,7 +21,11 @@ function nextSequence(){
     //Select the random color button and animate it
     $("#" + randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
 
-    //Play the sound of the correspondent button
-    var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+    playSound(randomChosenColour);
+}
+
+//Play the sound of the correspondent button
+function playSound(name){
+    var audio = new Audio("sounds/" + name + ".mp3");
     audio.play();
 }
