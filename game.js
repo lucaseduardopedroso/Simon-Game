698b6640e8ac2467d5f7c18976fcb59f57bdb121
lucaseduardopedroso <lucaseduardosilvapedroso@gmail.com>
2021-06-01 00:56:33 -0300

    Add game start and level titles

--- game.js
+++ game.js
@@ -1,6 +1,18 @@
 var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
+var started = 0;
+var level = 0;
+
+//Check if a key has been pressed to start
+$("body").keypress(function(){
+    if(!started){
+        //Change the h1 to the level
+        $("#level-title").text("Level " + level);
+        nextSequence();
+        started = 1;
+    }
+});
 
 //Check the button click and store its color (id)
 $(".btn").click(function(){
@@ -8,19 +20,22 @@ $(".btn").click(function(){
     //Store the color clicked in userClickedPattern
     userClickedPattern.push(userChosenColour);
     playSound(userChosenColour);
+    animatePress(userChosenColour);
 });
 
 function nextSequence(){
+    //Increase a level every time nextSequence is called and chage the level title
+    level++;
+    $("#level-title").text("Level " + level);
     //Generate a number between 0 - 3 and store it
     var randomNumber = Math.floor(Math.random() * 4);
     //Choose a random color from buttonColours using the random number
     var randomChosenColour = buttonColours[randomNumber];
     //Store the random color in the gamePattern
     gamePattern.push(randomChosenColour);
-
+    //Increase a level each 
     //Select the random color button and animate it
     $("#" + randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-
     playSound(randomChosenColour);
 }
 
@@ -28,4 +43,12 @@ function nextSequence(){
 function playSound(name){
     var audio = new Audio("sounds/" + name + ".mp3");
     audio.play();
+}
+
+//Add animation to the user click
+function animatePress(currentColour){
+    $("#" + currentColour).addClass("pressed");
+    setTimeout(function(){
+        $("#" + currentColour).removeClass("pressed");
+    }, 100);
 }
