401c11dc0e51026d70f35a8c9e6678faebcd1188
lucaseduardopedroso <lucaseduardosilvapedroso@gmail.com>
2021-05-31 23:33:48 -0300

    Add animations, sounds and color sequence

--- game.js
+++ game.js
@@ -2,10 +2,17 @@ var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 
 function nextSequence(){
-    //Generates a number between 0 - 3 and stores it.
+    //Generate a number between 0 - 3 and store it.
     var randomNumber = Math.floor(Math.random() * 4);
     //Choose a random color from buttonColours using the random number
     var randomChosenColour = buttonColours[randomNumber];
-    //Stores the random color in the gamePattern
+    //Store the random color in the gamePattern
     gamePattern.push(randomChosenColour);
+
+    //Select the random color button and animate it
+    $("#" + randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+
+    //Play the sound of the correspondent button
+    var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+    audio.play();
 }
